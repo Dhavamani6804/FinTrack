8f75afa941b3b91e95a59088b4cbb3a5d7ef1f2e
Dhavamani6804 <sdhavamani22@gmail.com>
2026-06-25 22:39:26 +0530

    DEBUG CORS

--- server/server.js
+++ server/server.js
@@ -49,3 +49,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+console.log("CLIENT_URL =", process.env.CLIENT_URL);
+console.log("NODE_ENV =", process.env.NODE_ENV);
